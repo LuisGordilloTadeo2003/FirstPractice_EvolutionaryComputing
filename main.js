b56eddd540b26afb439ece13075cd7d501ffaafe
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-11-06 17:08:13 +0000

    completado

--- main.js
+++ main.js
@@ -1,18 +1,54 @@
 let poblation = [];
-const TARGET = "lorem ipsum";
-const PoblationSize = 1000;
+const TARGET = "El codigo bien escrito facilita el trabajo en equipo y ayuda a ¡PROSPERAR!.";
+const PoblationSize = 5000;
 
 let randomWord;
+let generation = 0;
 
 function main() {
     generatePoblation();
 
     //console.log(poblation);
 
-    childPoblation(poblation)
+    if (!isCorrect(poblation)) console.log("Generando...")
+
+    do {
+        poblation = childPoblation(poblation)
+        mutation(poblation);
+        generation++;
+
+        let maxFitness = Math.max(...poblation.map(obj => obj.fitness))
+
+        console.log("Generacion: " + generation)
+        console.log(poblation.find(obj => obj.fitness === maxFitness).word)
+        //if (generation % 100 == 0) console.log(generation + ". " + poblation[49].word);
+    } while (!isCorrect(poblation))
+}
+
+function mutation(poblation) {
+    let mutationRate = 0.1;
+
+    if (Math.random() < mutationRate) {
+        let index = Math.floor(Math.random() * poblation.length);
+        poblation[index].word[Math.floor(Math.random() * TARGET.length)] = randomCharacter();
+    }
+}
+
+function isCorrect(poblation) {
+    for (let i = 0; i < poblation.length; i++) {
+        if (poblation[i].word == TARGET) {
+            console.log(generation)
+            console.log(poblation[i].word);
+            return true;
+        }
+    }
+
+    return false;
 }
 
 function childPoblation(poblation) {
+    let childPoblation = [];
+
     for (let i = 0; i < PoblationSize; i++) {
         let childA = ruleta(poblation);
         let childB = ruleta(poblation);
@@ -20,7 +56,23 @@ function childPoblation(poblation) {
         let crossover = Math.floor(Math.random() * TARGET.length)
 
         randomWord = childA.word.substring(0, crossover) + childB.word.substring(crossover, childB.word.length);
+
+        let fit = fitness(randomWord);
+
+        childPoblation.push(
+            {
+                id: i,
+                word: randomWord,
+                fitness: fit
+            }
+        )
+
+        //console.log(poblation[i], childPoblation[i])
     }
+    //poblation = childPoblation;
+    poblation = [];
+    //console.log(childPoblation)
+    return childPoblation;
 }
 
 function generatePoblation() {
